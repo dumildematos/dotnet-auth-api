a12263132eac74a6109dc34c16b00da62c407cbf
Dumilde Matos <dumildejosematos@gmail.com>
2023-11-13 14:02:25 +0000

    first commit

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dotnet_auth_api.Controllers
+{
+	[ApiController]
+	[Route("")]
+	public class AuthController: Controller
+	{
+		[HttpGet]
+		public IActionResult Hello()
+		{
+			return Ok("Sucess!");
+		}
+	}
+}
+
